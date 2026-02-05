8532755a70bd08022da11165cb4380f5f26486fc
Naveen <chnaveenprakash@gmail.com>
2026-02-05 22:25:07 +0530

    Playing with Routes

--- src/app.js
+++ src/app.js
@@ -3,25 +3,41 @@ const express = require("express");
 
 const app = express();
 
-// This will match only GET requests to /user
+// Accepts any req if it is /user,/user/xyz,/user/abc/def
 app.get("/user", (req, res) => {
   res.send({firstName: 'naveen', lastName: 'Prakash'});
 });
 
-// This will match only POST requests to /user
-app.post("/user", (req, res) => {
-  res.send("Data successfully saved to DB");
+// Accepts any req if it is /ac,/abc 
+// (ab?c worked until express 4.x.x, but in express 5.x.x it is not working, so we can use regex to achieve the same result)
+app.get(/^\/ab?c$/, (req, res) => {
+  res.send({firstName: 'naveen', lastName: 'Prakash'});
+});
+app.get("/user", (req, res) => {
+  console.log(req.query); // to get optional filters and modifiers or query parameters
+  res.send({firstName: 'naveen', lastName: 'Prakash'});
 });
 
-// This will match only DELETE requests to /user
-app.delete("/user", (req, res) => {
-  res.send("Data successfully deleted from DB");
+// this is for dynamic routing
+app.get("/user/:id", (req, res) => {
+  console.log(req.params); // to get required route data
+  res.send({firstName: 'naveen', lastName: 'Prakash'});
 });
 
-// This will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello World from test");
-});
+// // This will match only POST requests to /user
+// app.post("/user", (req, res) => {
+//   res.send("Data successfully saved to DB");
+// });
+
+// // This will match only DELETE requests to /user
+// app.delete("/user", (req, res) => {
+//   res.send("Data successfully deleted from DB");
+// });
+
+// // This will match all the HTTP method API calls to /test
+// app.use("/test", (req, res) => {
+//   res.send("Hello World from test");
+// });
 
 // app.use("/", (req, res) => {
 //   res.send("Hello World update");
