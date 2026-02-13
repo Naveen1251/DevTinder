f9d6a19dcba819e9525ab208b9e0fa6fc73223c7
Naveen <chnaveenprakash@gmail.com>
2026-02-13 17:18:06 +0530

    schema-validations

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
-app.use(express.json()) //This middleware helps to convert json object into JS object and set into req body 
+app.use(express.json()); //This middleware helps to convert json object into JS object and set into req body
 // without above middleware if you clg(req.body), this will return undefined.
 
 app.post("/signup", async (req, res) => {
-
+  // 
   // creating a new instance of user model
   const user = new User(req.body);
   try {
@@ -18,6 +18,55 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("Error saving the user");
   }
 });
+
+// get user by firstname
+app.get("/user", async (req, res) => {
+  const userFirstName = req.body.firstName;
+  try {
+    const users = await User.find({ firstName: userFirstName });
+    if (users.length === 0) {
+      res.status(404).send("User Not Found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+
+// Get data of all users available in DB
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+
+
+// Delete user
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User Deleted sucessfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Patch
+app.patch("/user",async (req,res)=>{
+  const lastname = req.body.LastName;
+  const data = req.body
+  try{
+    const updatedUser = await User.findOneAndUpdate({LastName:lastname},data)
+    res.send("User Updated Successfully")
+  }catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+})
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -2,25 +2,45 @@ const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({ //if we dont right new also it works fine
     firstName:{   //firstName can be also written as first_name,firstname anything will work
-        type: String
+        type: String,
+        required: true,
+        minlength: 4, //if we want to set minimum length of name
+        maxlength: 30 //if we want to set maximum length of name
     },
     lastName:{
         type: String
     },
     emailId:{
-        type: String
+        type: String,
+        required:true,
+        unique: true,
+        lowercase: true //this will convert email to lowercase before saving in database
     },
     password:{
         type: String
     },
     age:{
-        type: Number
+        type: Number,
+        min: 18, //if we want to set minimum age limit
     },
     gender: {
-        type: String
+        type: String,
+        validate(value){
+            if(!["male","female","other"].includes(value)){
+                throw new Error("Invalid Gender Value")
+            }
+        }
+    },
+    photoUrl:{
+        type: String,
+        default: "https://www.kindpng.com/picc/m/451-4517876_default-profile-hd-png-download.png" //if user dont upload any photo then this default photo will be used
+    },
+    about:{
+        type: String,
+        default: "Hey there! I am using this app." //if user dont write anything in about section then this default about will be used
     }
 
-})
+},{timestamps: true}) //this will automatically add createdAt and updatedAt field in our database
 
 const User = mongoose.model("user",userSchema); //First parameter is name of model,second is schema of model
 // User should always start with capital letter
