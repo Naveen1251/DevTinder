a156c9a8af90a991d202e458b53f09a394476e2c
Naveen <chnaveenprakash@gmail.com>
2026-02-18 22:35:54 +0530

    query and indexes in DB

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,14 +1,56 @@
-const express = require("express")
+const express = require("express");
 const requestRouter = express.Router();
-const {userAuth} = require("../middlewares/auth")
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  try{
-    const user  = req.user
-    res.send(user.firstName + " sent you connection request" )
-  }catch(err){
-    err.status(400).send("Error sending connection: " + err.message);
-  }
-})
+requestRouter.post(
+  "/request/send/:status/:userId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.userId;
+      const status = req.params.status;
+
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        throw new Error("Invalid status value");
+      }
+
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        throw new Error("User not found");
+      }
+
+      const existingConnectionUser = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+      if (existingConnectionUser) {
+        throw new Error(
+          "Connection request already exists between these users",
+        );
+      }
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await connectionRequest.save();
+
+      res.json({
+        message: `${req.user.firstName} ${status === "interested" ? "is interested in" : "ignored"} ${toUser.firstName}`,
+        data,
+      });
+    } catch (err) {
+      res.status(400).send("Error sending connection: " + err.message);
+    }
+  },
+);
 
 module.exports = requestRouter;
